75ff6081dc0e61b42c2c351021cb5514a6f0361a
crullian <crullian@gmail.com>
2015-08-29 20:25:53 -0400

    Really removing ignored stuff now

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  twitter: {
-    consumer_key: 'WjE4mHF6CjXvQyMPgkR9RO1XS',
-    consumer_secret: 'E73KP1BwuOloFwAYXUPhlACJ7zLC6LNilcuL9RZYo7nl4HVxK5',
-    access_token_key: '1961550602-NWo8Npy0mGD8MCHDESZ71MQpzwqaAOtASGrdfHB',
-    access_token_secret: 'dUm5S4Zd6jtGlhky4GXajqBVsdZ6t9Gx0JESZfZwXTnRD'
-  }
-}
